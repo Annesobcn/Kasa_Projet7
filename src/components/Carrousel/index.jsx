724592b247b0e10df07342624e3eb1b8874f8666
Annesobcn <asm.professeur@gmail.com>
2022-12-12 19:10:24 +0100

    tentative avec carrousel de react mais si je remplace ma div wrapper par Slide ou Carrousel ça ne marche plus: retravailler

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -7,34 +7,24 @@ import { useParams } from 'react-router'
 import logements from '../../assets/logements.json'
 
 const ImageLogement = styled.img`
-  width: 90vw;
+  width: 1240px;
   height: 415px;
-  transform: translateY(-50%);
-  left: 10vw;
-  padding: 0.4rem 1.5rem;
-  background: #ff6060;
-  color: #ffffff;
-  position: absolute;
 `
 const Arrow = styled.button`
   wwidth: 46px;
   height: 79px;
 `
-const properties = {
-  preArrow: <Arrow {...Arrow}>{arrowForwardCarrousel}</Arrow>,
-  nextArrow: <Arrow {...Arrow}>{arrowBackwardCarrousel}</Arrow>,
-}
 
 function Carrousel() {
   return (
-    <Slide {...properties}>
+    <div>
       {logements &&
         logements.map((slide) => (
           <div key={slide.id}>
             <ImageLogement src={slide.pictures} alt="propriété" />
           </div>
         ))}
-    </Slide>
+    </div>
   )
 }
 export default Carrousel

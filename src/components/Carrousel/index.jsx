e767ec2a6b6c3fc04a60ec6fcc5a9e2defaea6ce
Annesobcn <asm.professeur@gmail.com>
2022-12-13 13:04:12 +0100

    travail sur carrousel: probleme d'affichage> a corriger

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -10,21 +10,45 @@ const ImageLogement = styled.img`
   width: 1240px;
   height: 415px;
 `
-const Arrow = styled.button`
+const ArrowForward = styled.button`
   width: 46px;
   height: 79px;
+  background: none;
+  border: 0px;
+  color: white;
 `
+const ArrowBackward = styled(ArrowForward)
 
-function Carrousel() {
-  return (
-    <div>
-      {logements &&
-        logements.map((slide, index) => (
-          <div key={slide.id}>
-            <ImageLogement src={`${slide.pictures}-${index}`} alt="propriété" />
-          </div>
-        ))}
-    </div>
-  )
+const ImagePropriete = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  background-size: cover;
+  width: 1240px;
+  height: 415px;
+`
+const properties = {
+  prevArrow: <ArrowForward>{arrowForwardCarrousel}</ArrowForward>,
+  nextArrow: <ArrowBackward>{arrowBackwardCarrousel}</ArrowBackward>,
+}
+const Carrousel = (props) => {
+  const { id } = useParams()
+  const [pictures, setPictures] = useState([])
+
+  useEffect(() => {
+    const filterPictures = logements.find((logement) => logement.id === id)
+      .pictures
+    setPictures(filterPictures)
+  }, [id])
+
+  const affichagePictures = pictures.map((picture, index) => (
+    <ImagePropriete
+      {...properties}
+      key={index}
+      src={picture}
+      alt={picture.title}
+    />
+  ))
+  return <Slide>{affichagePictures}</Slide>
 }
 export default Carrousel

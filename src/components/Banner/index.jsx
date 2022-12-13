68b4248f47e4e16a7c3b9cebde51f83e0dfb917d
Annesobcn <asm.professeur@gmail.com>
2022-12-13 22:17:43 +0100

    css et responsive du composant banner ok

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,52 +1,12 @@
-import styled from 'styled-components'
-import colors from '../../utils/style/colors'
+import * as style from './style.module.css'
 import BackgroundImage from '../../assets/BackgroundSectionHeader1.svg'
-
-const BannerContainer = styled.div`
-  position: relative;
-  background-color: ${colors.secondary};
-  border-radius: 25px;
-  height: 223px;
-  width: 90vw;
-  max-width: 1240px;
-  font-size: 16px;
-
-  @media (max-width: 375px) {
-    width: 335px;
-    height: 111px;
-    border-radius: 10px;
-  }
-`
-const BannerImage = styled.img`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 25px;
-  filter: brightness(0.7);
-  @media (max-width: 375px) {
-    border-radius: 10px;
-  }
-`
-const BannerText = styled.span`
-  position: absolute;
-  text-align: center;
-  top: 35%;
-  width: 90vw;
-  font-family: montserrat;
-  font-size: 3rem;
-  color: white;
-  @media (max-width: 375px) {
-    font-size: 24px;
-  }
-`
 
 function Banner() {
   return (
-    <BannerContainer>
-      <BannerImage img src={BackgroundImage} />
-      <BannerText>Chez vous, partout et ailleurs</BannerText>
-    </BannerContainer>
+    <div className={style.bannercontainer}>
+      <img className={style.bannerimage} src={BackgroundImage} />
+      <span className={style.bannertext}>Chez vous, partout et ailleurs</span>
+    </div>
   )
 }
 
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,79 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const CardWrapper = styled.div`
-  max-width: 340px;
-  max-height: 340px;
-  border-radius: 10px;
-  margin: 10px;
-  position: relative;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  @media (max-width: 375px) {
-    width: 333px;
-    max-height: 255px;
-    border-radius: 10px;
-    margin: 5px 0 5px 0;
-  }
-`
-const CardImage = styled.img`
-  height: 340px;
-  max-width: 340px;
-  object-fit: cover;
-  align-self: center;
-  border-radius: 10px;
-  @media (max-width: 375px) {
-    max-with: 332px;
-    max-height: 255px;
-    border-radius: 25px;
-  }
-`
-const CardOmbre = styled.div`
-  background: rgb(11, 11, 11);
-  background: linear-gradient(
-    0deg,
-    rgba(11, 11, 11, 1) 0%,
-    rgba(255, 255, 255, 0.006711409395973145) 50%
-  );
-  border-radius: 10px;
-  height: 100%;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100%;
-
-  @media (max-width: 375px) {
-    max-with: 335px;
-    max-height: 255px;
-    border-radius: 10px;
-  }
-`
-
-const CardTitle = styled.p`
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
-  margin-right: 6px;
-  color: #ffffff;
-  font-family: montserrat;
-  font-size: 18px;
-  font-weight: 500;
-  @media (max-width: 375px) {
-    left: 4vw;
-    top: 23vh;
-  }
-`
+import * as style from './style.module.css'
 
 const Card = ({ picture, title }) => {
   return (
-    <CardWrapper>
-      <CardImage src={picture} alt={title} />
-      <CardOmbre></CardOmbre>
-      <CardTitle>{title}</CardTitle>
-    </CardWrapper>
+    <div className={style.cardwrapper}>
+      <img className={style.cardimage} src={picture} alt={title} />
+      <div className={style.cardombre}></div>
+      <p className={style.cardtitle}>{title}</p>
+    </div>
   )
 }
 

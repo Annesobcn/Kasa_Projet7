0d2945907267b19c95cdfe5500b05f543bba392d
Annesobcn <asm.professeur@gmail.com>
2022-12-14 16:33:32 +0100

    css et responsive de la page accueil ok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,57 +1,15 @@
 import Banner from '../../components/Banner'
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Card from '../../components/Card'
 import logements from '../../assets/logements.json'
-import colors from '../../utils/style/colors'
-
-const HomeContainer = styled.div`
-  display: flex;
-  width: 100vw;
-  max-width: 1440px;
-  padding-top: 5vh;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  overflow: auto;
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none;
-  @media (max-width: 375px) {
-    padding-left: 10px;
-    max-width: 336px;
-    padding-top: 0;
-  }
-`
-
-const CardsContainer = styled.div`
-  max-width: 1145px;
-  margin: 4vh;
-  width: 90vw;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  padding: 5vh 3vw;
-  border-radius: 25px;
-  background-color: ${colors.secondary};
-
-  @media (max-width: 375px) {
-    max-width: 335px;
-    border-radius: 10px;
-    margin: 20px;
-    padding: 1vh 1vw;
-    margin: 0.1vh 1vw;
-    background-color: #ffffff;
-    border-radius: 10px;
-  }
-`
+import * as style from './style.module.css'
 
 function Home() {
   return (
-    <HomeContainer>
+    <div className={style.homecontainer}>
       <Banner />
 
-      <CardsContainer>
+      <div className={style.cardscontainer}>
         {logements.map((logement) => (
           <Link to={'/fichelogement/' + logement.id + '/#'} key={logement.id}>
             <Card
@@ -62,8 +20,8 @@ function Home() {
             />
           </Link>
         ))}
-      </CardsContainer>
-    </HomeContainer>
+      </div>
+    </div>
   )
 }
 
